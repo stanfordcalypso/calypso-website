05c9dc23e7fdfc04468d21e93a9f0f3b2dbd4218
stanfordcalypso <stanfordcalypso@gmail.com>
2012-07-28 08:08:12 +0900

    Update content/home.php

--- content/home.php
+++ content/home.php
@@ -11,7 +11,7 @@
   <td class='half'>
   <h1>Welcome</h1>
     <p>Cardinal Calypso is Stanford University's <a target='_blank' href='http://en.wikipedia.org/wiki/Steelpan'>steelpan</a> ensemble. Founded in 2005, it may be seen anywhere around the greater Stanford campus, causing wanton happiness and general excitement.</p>
-   <p>On this website, you can find more information about the <a href='?p=group'>history, members and repertoire</a> of the group; <a href='?p=pans'>the instruments</a> we play and the culture they embody; or <a href='?p=booking'>booking</a> us for an event. You can also investigate <a href='?p=media'>recordings and photos</a> of the group, including <a href='?p=premieres'>original music we've written</a>.</p>
+   <p>On this website, you can find more information about the <a href='?p=group'>history, members and repertoire</a> of the group; <a href='?p=pans'>the instruments</a> we play and the culture they embody; or <a href='?p=booking'>booking</a> us for an event. You can also investigate <a href='?p=media'>recordings and photos</a> of the group, including <a href='?p=premieres'>original music we've written</a>..</p>
   </td>
   <td class='half'>
     <h1>Booking</h1>
