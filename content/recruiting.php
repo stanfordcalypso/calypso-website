7a605756f06390787ed11860ec08abddce60cfec
stanfordcalypso <stanfordcalypso@gmail.com>
2012-07-28 07:11:37 +0900

    Update content/recruiting.php

--- content/recruiting.php
+++ content/recruiting.php
@@ -1,18 +1,16 @@
 <table cellspacing='0' cellpadding='0'><tr>
   <td class='half'>
   <h1>Join Cardinal Calypso</h1>
-    <p>Cardinal Calypso is searching for three talented Stanford students to join our band this school year. If you love music and sunshine and want to be a part of a friendly and exciting student group, we would love for you to try out.</p>
-    <p>We'll be holding an informational meeting for anybody interested in joining on <b>Sunday, September 26, 5pm, in Roble theater (inside Roble dorm)</b>. We'll field any questions you have, detail the audition process, and offer you the chance to sign up for a one-on-one audition later in the week. You don't commit to anything by showing up at the meeting-- it's just a way for us to meet people and for you to meet us. Bring your friends!</p>
+    <p>Cardinal Calypso is searching for four talented Stanford students to join our band this school year. If you love music and sunshine and want to be a part of a friendly and exciting student group, we would love for you to try out.</p>
+    <p>Come find us at the Activities Fair on Friday, September 28</b>. We'll field any questions you have, detail the audition process, and offer you the chance to sign up for a one-on-one audition later in the week. </p>
   </td>
   <td class='half'>
     <h1>Sweet Details</h1>
     <ul>
       <li>No previous steel drum experience is necessary. Bring your musicianship.</li>
-      <li>We meet for four hours a week (tentatively Sundays 6-8pm and Tuesdays 7-9pm).</li>
+      <li>We meet for four hours a week (tentatively Sundays 4-6pm and Tuesdays 7-9pm).</li>
       <li>We play gigs a few times a month and have a rocking spring show.</li>
     </ul>
-    <h1>Or: Play the Drumset</h1>
-    <p>We're also looking for a solid, versatile drummer to become the engine room for our band. The group functions similarly to a jazz combo, and you get free access to our drumset.</p>
   </td></tr>
   <tr><td class='full' colspan='2'><a title='See more photos' href='?p=media'><img src='photos/calypso05.jpg' alt='Cardinal Calypso' /></a></td></tr>
 </table>
