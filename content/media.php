1be149840a76b8ad6653a667ef564ace55775a54
Cardinal Calypso <stanfordcalypso@gmail.com>
2012-08-04 13:09:48 +0900

    updated media page

--- content/media.php
+++ content/media.php
@@ -6,15 +6,21 @@
 			<a href='music/here_comes_the_sun.mp3'>The Beatles - Here Comes the Sun</a> (160kbps mp3, 3:05)<br />
 			<a href='music/bohemian_rhapsody.mp3'>Queen - Bohemian Rhapsody, second half</a> (160kbps mp3, 3:00)
 		</p>
+        </td></tr>
+        <tr><td class='half'>
 		<h1>Videos</h1>
 		<p>
-			<iframe width="392" height="221" src="http://www.youtube.com/embed/ziAlbHqS27E" frameborder="0" allowfullscreen></iframe><br />
+			<iframe width="392" height="221" src="http://www.youtube.com/watch?v=yUfXOv5MCeI" frameborder="0" allowfullscreen></iframe><br />
+            <span class='caption'>Spring Show, 2012 - Jerk it Out (The Caesars)</span><br /><br />
+            <iframe width="392" height="221" src="http://www.youtube.com/embed/ziAlbHqS27E" frameborder="0" allowfullscreen></iframe><br />
    <span class='caption'>Spring Show, 2011 - Jamming (Bob Marley) / Epic Drum Solo / Pan in A Minor</span><br /><br />
 			<iframe width="392" height="221" src="http://www.youtube.com/embed/CNzGXiigpc0" frameborder="0" allowfullscreen></iframe><br />
 			<span class='caption'>Spring Show, 2011 - Autumn in Ganymede</span><br /><br />
 			<iframe width="392" height="221" src="http://www.youtube.com/embed/TQ9xp5dxnUU" frameborder="0" allowfullscreen></iframe><br />
 			<span class='caption'>Faces, 2010 - The Hammer</span>
 		</p>
+        </td>
+        <td class='half'>
 		<h1>Photos</h1>
 		<p>
 			<img src='photos/calypso07.jpg' alt='Cardinal Calypso' /><br />
