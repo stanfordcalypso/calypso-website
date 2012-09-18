082b1cc3f78ef798611dfa5b7900bf62203cf54a
Cardinal Calypso <stanfordcalypso@gmail.com>
2012-09-18 07:41:44 -0700

    updated recruiting page

--- content/recruiting.php
+++ content/recruiting.php
@@ -1,16 +1,24 @@
 <table cellspacing='0' cellpadding='0'><tr>
   <td class='half'>
   <h1>Join Cardinal Calypso</h1>
-    <p>Cardinal Calypso holds auditions once per year at the beginning of fall quarter, and this year we are searching for four talented Stanford students to join our band! If you love music and sunshine and want to be a part of a friendly and exciting student group, we would love for you to try out.</p>
-    <p>Come find us at the Activities Fair on <b>Friday, September 28, from 12-4 PM</b>! Audition details to come soon.</p>
-  </td>
-  <td class='half'>
-    <h1>Sweet Details</h1>
+    <p>Cardinal Calypso holds auditions once per year at the beginning of fall quarter, and this year we are searching for four talented Stanford students to join our band! If you love music and sunshine and want to be a part of a friendly and exciting student group, we would love for you to try out.
     <ul>
       <li>No previous steel drum experience is necessary. Bring your musicianship.</li>
-      <li>We meet for four hours a week (tentatively Sundays 4-6pm and Tuesdays 7-9pm).</li>
+      <li>We meet for four hours a week (tentatively Sundays 4-6pm and one other weekday in the evening).</li>
       <li>We play gigs a few times a month and have a rocking spring show.</li>
     </ul>
+    </p>
+  </td>
+  <td class='half'>
+  	<h1>Want More Info?</h1>
+    <p>
+    <ul>
+      <li>Information Session on Thursday, September 27 in Roble Theater. Time TBA.</li>
+      <li>Activities Fair on Friday, September 28 in White Plaza from 12-4 PM.</li>
+    </ul>
+    </p>
+    <h1>Fall 2012 Auditions</h1>
+    <p>We will be holding auditions on <b>Tuesday, October 2</b> and <b>Wednesday, October 3</b> in Roble Theater. Times TBA. Sign up for an audition slot at the Information Session, the Activities Fair, or email us at <b>stanfordcalypso at gmail dot com</b>.</p>
   </td></tr>
   <tr><td class='full' colspan='2'><a title='See more photos' href='?p=media'><img src='photos/calypso05.jpg' alt='Cardinal Calypso' /></a></td></tr>
 </table>
