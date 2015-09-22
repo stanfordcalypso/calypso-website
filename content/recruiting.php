9cf4bd1d7518244dcf60fa1360e0f7433fa1c85d
tleavitt <tucker.leavitt@gmail.com>
2015-09-22 12:23:15 -0700

    Added information about Picnic and Pan event at the Oval

--- content/recruiting.php
+++ content/recruiting.php
@@ -1,5 +1,11 @@
 <table cellspacing='0' cellpadding='0'><tr>
   <td class='half'>
+     <div class='notice'>
+      <h1>We Want to Meet You!</h1>
+      <p>Interested in auditioning?<a href="https://docs.google.com/spreadsheets/d/1d5lnwMIJSgTMbl5avicPnsqJHDgb5t8qf0AvnV_Hw94/edit">SIGN UP HERE!</a> Or, to learn more about the group:</p>
+      <p> Find us at the Activities Fair from <b>noon to 4pm</b> on <b>Friday, September 25, 2015</b> in White Plaza!</p>
+      <p> Come to our Picnic and Pan meet-and-greet with other prospective Calypsonians from <b>2 to 4pm</b> on <b>Saturday, September 26, 2015</b> at the Oval!</p>
+    </div> 
   <h1>Join Cardinal Calypso</h1>
     <p>Cardinal Calypso holds auditions once per year at the beginning of fall quarter. If you love music and sunshine and want to be a part of a friendly and exciting student group, we would love for you to try out.
     <ul>
@@ -15,7 +21,7 @@
     <h1>Want More Info?</h1>
     <ul>
       <li>Come find us at the Activities Fair from <b>noon to 4pm</b> on <b>Friday, September 25, 2015</b> in White Plaza! We can answer whatever questions you have, including any about auditions!</li>
-      <!-- <li>There will be an <b>Information Session</b> (tentatively on <b>Saturday, September 28 in Manzanita Dining</b>) where you can come and ask questions if you didn't catch us at the Activities Fair.</li> -->
+      <li>We will hold an informal Meet-and-Greet from <b>2 to 4pm</b> on <b>Saturday, September 26, 2015</b> at the Oval where you can come and ask questions and meet the group if you didn't catch us at the Activities Fair.</li>
       <li>Feel free to email us at <b><a href="mailto:stanfordcalypso@gmail.com">stanfordcalypso@gmail.com</a></b>.</li>
     </ul>
   </td>
@@ -25,7 +31,7 @@
     <p>Auditions will be held on:</p>
 	  <p><b>Sunday, September 27 from noon to 6pm</b> at <b>Braun Music Center, rooms 105 and 106</b>, and</p>
 	  <p><b>Monday, September 28 from 7:30 to 10:30pm</b> at <b>Braun Music Center, rooms 103 and 105</b>.</p>
-    <p>Sign up to AUDITION at <a href="https://docs.google.com/spreadsheets/d/1d5lnwMIJSgTMbl5avicPnsqJHDgb5t8qf0AvnV_Hw94/edit">THIS LINK!</a> Please only sign up for one slot. During the actual audition, we'll interview you (just an informal get-to-know-you) and teach you a song on pan. We will let you know about callbacks a few days after auditions.</p>
+    <p>Sign up to AUDITION at <a href="https://docs.google.com/spreadsheets/d/1d5lnwMIJSgTMbl5avicPnsqJHDgb5t8qf0AvnV_Hw94/edit">THIS LINK!</a> Please only sign up for one slot. During the actual audition, we'll interview you (just an informal get-to-know-you) and teach you a song on pan (drummers: we'll make you play a few beats for us too). We will let you know about callbacks a few days after auditions.</p>
     </td></tr>
   <tr><td class='full' colspan='2'></td></tr>
 </table>
