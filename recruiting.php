67aa17448f3dc15458a51af56c2fd62f41ddf90f
zbirnholz1 <zacharybirnholz@gmail.com>
2018-09-25 13:10:06 -0700

    Added auditions 2k18!!!!!

--- recruiting.php
+++ recruiting.php
@@ -47,13 +47,13 @@
 				<table cellspacing='0' cellpadding='0'><tr>
  				    <div class='notice'>
 				      <h1>We Want to Meet You!</h1>
-				      <p>Interested in auditioning? <a href="https://docs.google.com/spreadsheets/d/1mgSJqRkI9EAOmMnwZ18JUqakjs5t1ueSp3qe5xtG1qU/edit">SIGN UP HERE!</a> Or, to learn more about the group:</p>
-				      <p> Find us at the Activities Fair from <b>noon to 4pm</b> on <b>Friday, September 29, 2017</b> in White Plaza!</p>
+				      <p>Interested in auditioning? <a href="https://docs.google.com/spreadsheets/d/112Yv6t-9KdcXtzQMeUu1mrMRVUNP5-IfzMwtQ8mLFeU/edit?usp=sharing">SIGN UP HERE!</a> Or, to learn more about the group:</p>
+				      <p> Find us at the Activities Fair from <b>noon to 4pm</b> on <b>Friday, September 28, 2018</b> in White Plaza!</p>
 				    </div>
 				    <p>Cardinal Calypso holds auditions once per year at the beginning of fall quarter. If you love music and sunshine and want to be a part of a friendly and exciting student group, we would love for you to try out.
 				    <ul>
 				      <li>No previous steel drum experience is required or expected, but bring your best musicianship!</li>
-				      <li>We meet for four hours a week (Sundays 3-5pm and Thursdays 8-10pm), plus one optional practice on Tuesday nights.</li>
+				      <li>We meet for four hours a week (Sundays 3-5pm and Thursdays 9-11pm), plus one optional practice on Tuesday nights.</li>
 				      <li>We play gigs a few times a month around campus and the Bay Area.</li>
 				      <li>We put on a rocking spring show every year featuring original music and crazy costumes.</li>
 				    </ul>
@@ -62,15 +62,15 @@
 				      <b>Drummers</b>: are you looking for a unique musical opportunity? Sign up for an audition!
 				    </p>
 				      <ul><li>Desirements: experience playing with a group and an appreciation for calypso music. </li></ul>
-				    <p style="margin-bottom: 0;">Auditions will be held at  <b>Dinkelspiel Rehearsal Hall</b> on:</p>
+				    <p style="margin-bottom: 0;">Auditions will be held at the <b>Braun Music Center</b> on:</p>
 					<ul>
-					  <li><b>Sunday, October 1 from 11am to 5pm</b>, and</li>
-					  <li><b>Monday, October 2 from 6:30 to 9:30pm</b>.</li>
+					  <li><b>Sunday, September 30 from 11:10am to 5:20pm</b>, and</li>
+					  <li><b>Monday, October 1 from 6:00pm to 7:20pm</b>.</li>
 					</li>
-				    <p style="margin-top: 10px">Sign up to AUDITION at <a href="https://docs.google.com/spreadsheets/d/1mgSJqRkI9EAOmMnwZ18JUqakjs5t1ueSp3qe5xtG1qU/edit?usp=sharing">THIS LINK!</a> Please only sign up for one slot. During the actual audition, we'll interview you (just an informal get-to-know-you) and teach you a song on pan (drummers: we'll make you play a few beats for us too). We will let you know about callbacks a few days after auditions.</p>
+				    <p style="margin-top: 10px">Sign up to AUDITION at <a href="https://docs.google.com/spreadsheets/d/112Yv6t-9KdcXtzQMeUu1mrMRVUNP5-IfzMwtQ8mLFeU/edit?usp=sharing">THIS LINK!</a> Please only sign up for one slot. During the actual audition, we'll interview you (just an informal get-to-know-you) and teach you a song on pan (drummers: we'll make you play a few beats for us too). We will let you know about callbacks a couple days after auditions.</p>
 				    <h1>Want More Info?</h1>
 				    <ul>
-				      <li>Come find us at the Activities Fair from <b>noon to 4pm</b> on <b>Friday, September 29, 2017</b> in White Plaza! We can answer whatever questions you have, including any about auditions!</li>
+				      <li>Come find us at the Activities Fair from <b>noon to 4pm</b> on <b>Friday, September 28, 2017</b> in White Plaza! We can answer whatever questions you have, including any about auditions!</li>
 				      <li>Feel free to email us at <b><a href="mailto:stanfordcalypso@gmail.com">stanfordcalypso@gmail.com</a></b>.</li>
 				    </ul>
 
